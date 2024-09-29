197897d1c3f31f00a7a2fbccc83b426bf6c4d497
Ran Wang <wangran@bosc.ac.cn>
2024-09-29 18:21:19 +0800

    platform: generic: Fix relocated dtb destroy payload content issue in some cases
    For payload boot process, dtb will be relocated to $(FW_TEXT_START) + 0x2200000 by OpenSBI.
    
    Then the memory layout will be:
    +---------------+<---$(FW_TEXT_START)
    |    OpenSBI    |
    +---------------+<---$(FW_TEXT_START) + 0x200000
    |               |
    +    Payload    +
    |               |
    +---------------+<---$(FW_TEXT_START) + 0x2200000
    | Relocated DTB |
    +---------------+
    |               |
    +               +
    |               |
    +               +
    |               |
    +---------------+
    
    So, if payload size is bigger than 32MB(0x2000000), it will conflict with
    dtb and some of its content will be corruptted.
    
    To handle this, we could swap these 2 images (and make position adjustment) like:
    +---------------+<---$(FW_TEXT_START)
    |    OpenSBI    |
    +---------------+<---$(FW_TEXT_START) + 0x200000
    | Relocated DTB |
    +---------------+<---$(FW_TEXT_START) + 0x400000
    |               |
    +    Payload    +
    |               |
    +---------------+
    |               |
    +               +
    |               |
    +               +
    |               |
    +---------------+
    
    Please see docs/firmware/fw_payload.md for detailed information
    
    Signed-off-by: Ran Wang <wangran@bosc.ac.cn>

--- platform/generic/objects.mk
+++ platform/generic/objects.mk
@@ -32,13 +32,13 @@ else
   # This needs to be 2MB aligned for 64-bit system
   FW_JUMP_ADDR=$(shell printf "0x%X" $$(($(FW_TEXT_START) + 0x200000)))
 endif
-FW_JUMP_FDT_ADDR=$(shell printf "0x%X" $$(($(FW_TEXT_START) + 0x2200000)))
+FW_JUMP_FDT_ADDR=$(shell printf "0x%X" $$(($(FW_TEXT_START) + 0x200000)))
 FW_PAYLOAD=y
 ifeq ($(PLATFORM_RISCV_XLEN), 32)
   # This needs to be 4MB aligned for 32-bit system
   FW_PAYLOAD_OFFSET=0x400000
 else
   # This needs to be 2MB aligned for 64-bit system
-  FW_PAYLOAD_OFFSET=0x200000
+  FW_PAYLOAD_OFFSET=0x400000
 endif
 FW_PAYLOAD_FDT_ADDR=$(FW_JUMP_FDT_ADDR)
